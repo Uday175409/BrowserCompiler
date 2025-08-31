280c7295354bec948f9dddc94131cea5e575f9ff
Uday Chauhan <uday.chauhan0408@gmail.com>
2025-08-31 15:00:09 +0530

    Set the python version

--- App/DB/db.py
+++ App/DB/db.py
@@ -2,6 +2,8 @@
 
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient(
+    "mongodb+srv://udaychauhan0408:ZanF5IVCJNwYKMar@cluster1.4p9tezw.mongodb.net/NewsApp?retryWrites=true&w=majority&appName=Cluster1"
+)
 mongo_db = client["codingPlatform"]
 submissions_collection = mongo_db["submissions"]
